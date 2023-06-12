5aa777d35c944831e2b92138fdcd3d1276d9bd36
Anders bloch lauridsen <93762569+andersbloch09@users.noreply.github.com>
2023-06-12 12:18:02 +0200

    Update publisher_member_function.py

--- ros2_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ ros2_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -71,7 +71,7 @@ def main(args=None):
         #Receive data from the client
         data = conn.recv(1024)
         xml_string_recv = int.from_bytes(data, byteorder="little")
-        
+        xml_string_recv = str(xml_string_recv)
         #xml_string = '<?xml version="1.0" encoding="uft-8"?><info><ID>'+'7'+'</ID></info>'
     
         file_path = "/home/anders/ros2_ws/src/py_pubsub/py_pubsub/time.xml"
